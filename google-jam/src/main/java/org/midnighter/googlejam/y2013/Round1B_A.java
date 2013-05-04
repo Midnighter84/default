26e330ce55c60fb2b5a6e4acc51e954308cc01ff
Midnighter <midnighter.gateway@gmail.com>
2013-05-04 14:02:12 +0200

    Templates

---
+++ google-jam/src/main/java/org/midnighter/googlejam/y2013/Round1B_A.java
@@ -0,0 +1,42 @@
+package org.midnighter.googlejam.y2013;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Round1B_A
+{
+	static final String assignment = "C-small-attempt0";
+	static final String inFileName = "files/" + assignment + ".in";
+	static final String outFileName = "files/"+assignment+".out";
+	
+    Scanner in;
+    PrintWriter out;
+   
+    public String oneCase()
+    {
+    	return "";
+    }
+   
+    public void run() throws Exception
+    {
+        in = new Scanner(new FileReader(inFileName));
+        out = new PrintWriter(new FileWriter(outFileName));
+        
+        int nCases = in.nextInt();
+        in.nextLine();
+        for (int c = 1; c <= nCases; c++)
+            out.println("Case #" + c + ": " + oneCase());
+       
+        out.flush();
+        out.close();
+        in.close();
+    }
+    
+   
+    public static void main(String[] args) throws Exception
+    {
+        new Round1B_A().run();
+    }
+}
